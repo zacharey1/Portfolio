fde29fd72dc8c0e2d2a441fa1191a661fe234fd9
zacharey1 <konohaha77@gmail.com>
2023-01-08 20:34:23 +0900

    js update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 const overlay = document.querySelector(".overlay");
 const modalClose = document.querySelector(".modal-close");
-const cardImg = document.querySelector(".card-img");
+const cardImg = document.querySelectorAll(".card-img");
 const modalImg = document.querySelector(".modal-img");
 
 function displayModal () {
     overlay.classList.remove("hidden");
 }
 
-cardImg.addEventListener('click', e => {
-    displayModal();
-    // e.target.modalImg.src = "e.target.cardImg.src";
-})
+cardImg.forEach(
+    img => {
+        img.addEventListener('click', e => {
+            displayModal();
+         
+        })
+    }
+)
 
 modalClose.addEventListener('click', e => {
     overlay.classList.add("hidden");  
